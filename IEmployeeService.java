98c62ff0900d75af153ea05f58c2c678dfd9f8ec
SudarshanPantangi <sudarshan.h.g7@gmail.com>
2017-11-27 22:25:41 -0500

    Add files via upload

---
+++ IEmployeeService.java
@@ -0,0 +1,7 @@
+package com.example.demo.service;
+
+public interface IEmployeeService {
+
+	public void updateEmployee(String status,String studendGander,Integer studendAge);
+	public void deleteEmployee(Integer studendId);
+}
